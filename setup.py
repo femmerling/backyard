2992b81b755ae7bdb783723ecd248213edbc97bc
Fauzan Erich Emmerling <erich@emfeld.com>
2013-09-08 07:19:01 +0700

    Finalized setup file

--- setup.py
+++ setup.py
@@ -25,24 +25,21 @@ use MySQL as your database
 """
 
 import os.path
-import platform
 
 from subprocess import call
 
-from config import BASEDIR, GARDEN_TOOLS
-
-current_platform = platform.system()
+from config import BASEDIR, ADDITIONAL_GARDEN_TOOLS
 
 #this is the path to the jog.py file which will act as the testing server
-jog_path = os.path.join(BASEDIR, 'jog.py')
+try_path = os.path.join(BASEDIR, 'try.py')
 #this is the path to the virtualenv file
 v_path = os.path.join(BASEDIR, 'virtualenv.py')
 #this is the path to the mow.py file which will act as the tornado based production server
-mow_path = os.path.join(BASEDIR, 'mow.py')
+blow_path = os.path.join(BASEDIR, 'blow.py')
 #this is the path to the hammock.py file which will act as the gunicorn based production server
-hammock_path = os.path.join(BASEDIR, 'hammock.py')
+fly_path = os.path.join(BASEDIR, 'fly.py')
 #this is the path to the yard.py file, the main framework tool file
-yard_path = os.path.join(BASEDIR, 'yard.py')
+yard_path = os.path.join(BASEDIR, 'yard')
 
 """
 This will auto adjust the python runtime being used by the file.
@@ -57,8 +54,6 @@ def update_environment(file_path):
 	update_file = open(file_path, 'r')
 	original_lines = update_file.readlines()
 	original_lines[0] = '#! backyard/bin/python\n'
-	if current_platform == 'Windows':
-		original_lines[0] = '#! backyard\Scripts\python\n'
 	update_file.close()
 	update_file = open(file_path, 'w')
 	for lines in original_lines:
@@ -66,65 +61,50 @@ def update_environment(file_path):
 
 	update_file.close()
 
+"""
+This is to fix the migrate versioning bug.
 
-# """
-# This is to fix the migrate versioning bug.
-# Will be removed when the bug on the original package is fixed
+"""
 
-# """
-
-# def tempfix_migrate():
-# 	print "\nFixing the migrate bug \n"
-# 	buggy_path = os.path.join(BASEDIR, 
-# 					 'backyard/lib/python2.7/site-packages/migrate/versioning/schema.py')
-# 	if current_platform == 'Windows':
-# 		buggy_path = os.path.join(BASEDIR, 
-# 					 'backyard\lib\site-packages\migrate\\versioning\schema.py')
-# 	buggy_file = open(buggy_path,'r')
-# 	original_lines = buggy_file.readlines()
-# 	original_lines[9] = "\n"
-# 	buggy_file.close()
-# 	update_file = open(buggy_path,'w')
-# 	for lines in original_lines:
-# 		update_file.write(lines)
-# 	update_file.close()
+def fix_migrate():
+	print "\nFixing the migrate bug \n"
+	buggy_path = os.path.join(BASEDIR, 
+					 'backyard/lib/python2.7/site-packages/migrate/versioning/schema.py')
+	buggy_file = open(buggy_path,'r')
+	original_lines = buggy_file.readlines()
+	original_lines[9] = "from sqlalchemy import exc as sa_exceptions\n"
+	buggy_file.close()
+	update_file = open(buggy_path,'w')
+	for lines in original_lines:
+		update_file.write(lines)
+	update_file.close()
 
 update_environment(yard_path)
-update_environment(mow_path)
-update_environment(jog_path)
-update_environment(hammock_path)
+update_environment(blow_path)
+update_environment(try_path)
+update_environment(fly_path)
 
-bin_base = 'backyard/bin/pip'
-if current_platform == 'Windows':
-	bin_base = 'backyard\Scripts\pip'
-	if not os.path.exists(os.path.join(BASEDIR, 'db\\')):
-		os.makedirs(os.path.join(BASEDIR, 'db\\'))
-	call(['python', v_path, 'backyard'])	
-else:
-	if not os.path.exists(os.path.join(BASEDIR, 'db/')):
-		os.makedirs(os.path.join(BASEDIR, 'db/'))
-	call(['python', v_path, 'backyard'])
-	call(['chmod', 'a+x', yard_path])
-	call(['chmod', 'a+x', mow_path])
-	call(['chmod', 'a+x', jog_path])
-	call(['chmod', 'a+x', hammock_path])
+pip = 'backyard/bin/pip'
+if not os.path.exists(os.path.join(BASEDIR, 'greenhouse/')):
+	os.makedirs(os.path.join(BASEDIR, 'greenhouse/'))
+call(['python', v_path, 'backyard'])
+call(['chmod', 'a+x', yard_path])
+call(['chmod', 'a+x', blow_path])
+call(['chmod', 'a+x', try_path])
+call(['chmod', 'a+x', fly_path])
 
 print '\nNew backyard created, now placing garden tools in the backyard\n'
 
 print 'Placing Flask in the backyard\n'
-call([bin_base, 'install', 'flask'])
+call([pip, 'install', 'flask'])
 print '\nFlask placed\n'
 
-# print 'Placing SQLAlchemy in the backyard\n'
-# call([bin_base, 'install', 'sqlalchemy'])
-# print '\nSQLAlchemy placed\n'
-
 print 'Placing Flask-SQLAlchemy in the backyard\n'
-call([bin_base, 'install', 'flask-sqlalchemy'])
+call([pip, 'install', 'flask-sqlalchemy'])
 print '\nFlask-SQLAlchemy placed\n'
 
 print 'Placing SQLAlchemy-Migrate in the backyard\n'
-call([bin_base, 'install', 'sqlalchemy-migrate'])
+call([pip, 'install', 'sqlalchemy-migrate'])
 print '\nSQLAlchemy-Migrate placed\n'
 print '##################################################'
 print '# IMPORTANT                                      #'
@@ -132,29 +112,29 @@ print '# to use SQLAlchemy with MySQL, PostgreSQL and   #'
 print '# Oracle, you need to install additional modules #'
 print '##################################################\n'
 
-# tempfix_migrate()
+fix_migrate()
 
 print 'A Tornado is coming to your backyard\n'
-call([bin_base, 'install', 'tornado'])
+call([pip, 'install', 'tornado'])
 print '\nTornado contained in the backyard\n'
 
 print 'A Green Unicorn is coming to your backyard\n'
-call([bin_base, 'install', 'gunicorn'])
+call([pip, 'install', 'gunicorn'])
 print '\nGreen Unicord Stays in the backyard\n'
 
 print '\nAll garden tools and creatures are complete\n'
 print '\nYour initial backyard is ready\n'
 
-if len(GARDEN_TOOLS) > 0:
+if len(ADDITIONAL_GARDEN_TOOLS) > 0:
 	print '\nWait, seems like you need some extra tools.\n'
-	for package in GARDEN_TOOLS:
+	for package in ADDITIONAL_GARDEN_TOOLS:
 		print 'Placing ' + package.title() + " in the backyard\n"
-		call([bin_base,'install',package])
+		call([pip,'install',package])
 		print "\n" + package.title() + " placed\n"
 
 	print '\nAll extra tools placed.\n'
 
-print '\nRun ./yard.py -h for full details on what you can do with your backyard.\n'
+print '\nRun ./yard help for full details on what you can do with your backyard.\n'
 print '\nEnjoy your time at the backyard and thank your for using!\n\n'
 
 # end of file
