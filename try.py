cee97c449e7a2478829e6fb55702a1671cdf6bc3
Fauzan Erich Emmerling <erich@emfeld.com>
2015-08-13 15:26:45 +0700

    Adjusted the test server to use the new config

--- try.py
+++ try.py
@@ -16,9 +16,9 @@ Please refer to yard help for more info.
 
 """
 from app import app
-from config import SERVER_PORT
+from config import user_config
 
-app.run(debug = True,port=SERVER_PORT)
+app.run(debug=True, port=user_config.SERVER_PORT)
 
 
 # end of file
